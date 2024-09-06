767a0a02be4cd1fca8fe8c7c6c1dfa79b51ec596
AbuArwa001 <khalfanathman12@yahoo.com>
2024-09-06 04:40:54 +0300

    Add new files and components for layout and sidebar functionality

---
+++ app/components/layout/content/Content.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Content() {
+  return <aside className="sidebar col-span-2/3"></aside>;
+}
+
+export default Content;
---
+++ app/components/layout/footer/Footer.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Jamia
+          Mosque Committee
+        </p>
+      </aside>
+    </footer>
+  );
+};
+export default Footer;
