8d52c5b969d5a20b68aa1d7d5db06fe9a4a2bb89
AbuArwa001 <khalfanathman12@yahoo.com>
2024-09-10 08:17:40 +0300

    Add edit button

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
-
+import { Pencil2Icon } from "@radix-ui/react-icons";
 interface props {
   params: {
     id: string;
@@ -14,23 +15,29 @@ const EvenDetails = async ({ params: { id } }: props) => {
       id: parseInt(id),
     },
   });
-  if (!event) {
-    notFound();
-  }
+  if (!event) notFound();
   return (
-    <div>
-      <Heading>EvenDetails</Heading>
-      <br />
-      <Heading as="h2">{event.name}</Heading>
-      <Flex gap={`2`} my={`3`}>
-        <p>{event.status}</p>
-        <p>Location: {event.location}</p>
-        <p>{event.date.toDateString()}</p>
-      </Flex>
-      <Card className="prose" mt={`3`}>
-        <ReactMarkdown>{event.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="3">
+      <Box>
+        <Heading>EvenDetails</Heading>
+        <br />
+        <Heading as="h2">{event.name}</Heading>
+        <Flex gap={`2`} my={`3`}>
+          <p>{event.status}</p>
+          <p>Location: {event.location}</p>
+          <p>{event.date.toDateString()}</p>
+        </Flex>
+        <Card className="prose" mt={`3`}>
+          <ReactMarkdown>{event.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/events/${event.id}/edit`}>Edit Event</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
