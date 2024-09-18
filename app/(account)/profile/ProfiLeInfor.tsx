4cbcde2f352cf20777d149d852e5435efdb6ae55
AbuArwa001 <khalfanathman12@yahoo.com>
2024-09-18 16:33:48 +0300

    Refactor AWS S3 configuration and add new S3 endpoints
    - Update next.config.mjs to include new S3 endpoints for Google and AWS
    - Add migration files to modify the users table in the database to include a profile_picture column
    - Increase the size of the profile_picture column in the users table
    - Update tsconfig.json to include a new file for AWS S3 API
    - Update package.json to include new dependencies for AWS S3
    - Add NextOfKeen component for account profile page
    - Add route file for AWS S3 API
    - Update globals.css to include print styles for the profile card
    - Update page.tsx to include printable section and print button
    - Add s3.tsx file for AWS S3 API

--- app/(account)/profile/ProfiLeInfor.tsx
+++ app/(account)/profile/ProfiLeInfor.tsx
@@ -11,10 +11,10 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 // Example Prisma update function
 const s3Client = new S3Client({
-  region: "eu-north-1",
+  region: process.env.AWS_REGION || "",
   credentials: {
-    accessKeyId: "AKIAUJRDVR7A7AZQ2Z4M",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
   },
 });
 export const uploadFile = async (
