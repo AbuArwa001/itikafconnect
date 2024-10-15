36a330dade6cf802e2f4f682dfdebd810f7594bd
AbuArwa001 <khalfanathman12@yahoo.com>
2024-10-15 20:41:30 +0300

    Refactor email sender configuration to use no-reply@athcongroup.tech as the sender address

--- utils/mails.tsx
+++ utils/mails.tsx
@@ -7,7 +7,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
     // from: "info@jamiamosque.co.ke",
-    from: "onboarding@resend.dev",
+    from: "no-reply@athcongroup.tech",
     to: email,
     subject: "Reset Your Password",
     html: `<p> CLick <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -18,7 +18,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 
   await resend.emails.send({
     // from: "info@jamiamosque.co.ke",
-    from: "onboarding@resend.dev",
+    from: "no-reply@athcongroup.tech",
     to: email,
     subject: "Confirm your Email",
     html: `<p> CLick <a href="${confirmLink}">here</a> to confirm email.</p>`,
@@ -35,7 +35,7 @@ export const sendProfileEmail = async (email: string) => {
 
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "no-reply@athcongroup.tech",
       to: "khalfanathman12@gmail.com",
       subject: "User Profile Information",
       html: `<p>Please find attached the profile information of user ID ${email}.</p>`,
